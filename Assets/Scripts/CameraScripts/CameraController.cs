27cf32016e9944a59f07a20980a45b375ca8f82d
captnw <williamngoc93@hotmail.com>
2021-01-14 19:30:10 -0800

    Created a working single player scene with basic movement and orthagonal camera (+camera scrolling works a bit). Multiplayer scene doesn't work b/c there isn't a main menu (also didn't implement camera for photon multiplayer yet). And some misc. changes.

---
+++ Assets/Scripts/CameraScripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chromaturgy.CameraScripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        // Allows the player control the zoom on their orthagonal, player camera via scrolling
+
+        [SerializeField] private float zoomAmount = .5f;
+        [SerializeField] private float minSize = 3f;
+        [SerializeField] private float maxSize = 10f;
+
+        private Camera MyCamera = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            MyCamera = transform.Find("PlayerCamera").gameObject.GetComponent<Camera>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (MyCamera)
+            {
+                Vector2 scrollInput = Input.mouseScrollDelta;
+                if (scrollInput.y > 0)
+                {
+                    MyCamera.orthographicSize -= zoomAmount;
+                    MyCamera.orthographicSize = (MyCamera.orthographicSize > minSize) ? MyCamera.orthographicSize : minSize;
+                }
+                else if (scrollInput.y < 0)
+                {
+                    MyCamera.orthographicSize += zoomAmount;
+                    MyCamera.orthographicSize = (MyCamera.orthographicSize < maxSize) ? MyCamera.orthographicSize : maxSize;
+                }
+            }
+        }
+    }
+}
+   
---
+++ Assets/Scripts/MovementScripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Chromaturgy.MovementScripts
+{
+    // This is meant for player movement and Photon
+    // particularly addressing the issue of each player controlling their own character
+
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviourPun
+    {
+        [SerializeField] private float movementSpeed = 0f;
+
+        private CharacterController controller = null;
+
+        // Start is called before the first frame update
+        void Start() => controller = GetComponent<CharacterController>();
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Only accept controls from the correct player
+            if (photonView.IsMine)
+            {
+                ProcessInput();
+            }
+        }
+
+        private void ProcessInput()
+        {
+            // Movement vector is normalized so that diagonal movement isn't faster
+            Vector3 movement = new Vector3
+            {
+                x = Input.GetAxisRaw("Horizontal"),
+                y = 0f,
+                z = Input.GetAxisRaw("Vertical")
+            }.normalized;
+
+            // Time.deltatime is already handled for us in PlayerController SimpleMove
+            controller.SimpleMove(movement * movementSpeed);
+        }
+    }
+}
+
