8ced5df87bc3696b286306e3dc3f53e6cecc07d2
captnw <williamngoc93@hotmail.com>
2021-01-16 11:09:32 -0800

    Added janky orthagonal camera (w.i.p) and basic character movement

---
+++ Chromaturgy/Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject PlayerToTrack;
+    
+    [SerializeField]
+    private Vector3 CameraOffset = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+
+    private void TrackPlayer()
+    {
+        if (PlayerToTrack)
+        {
+            transform.position = PlayerToTrack.transform.position + CameraOffset;
+            transform.LookAt(PlayerToTrack.transform);
+        }
+    }
+}
---
+++ Chromaturgy/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 0f;
+    [SerializeField] private float RunSpeed = 0f;
+
+    private CharacterController controller = null;
+
+    // Start is called before the first frame update
+    void Start() => controller = GetComponent<CharacterController>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessPlayerInput();
+    }
+
+    // Takes in player's iputs for movement
+    private void ProcessPlayerInput()
+    {
+        // Normalize movement vector, so that we don't move faster when
+        // we move diagonally
+        Vector3 movement = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        }.normalized;
+
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            controller.Move(movement * RunSpeed * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(movement * walkSpeed * Time.deltaTime);
+        }
+    }
+
+
+}
