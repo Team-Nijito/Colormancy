d5b7d075af95337f7c9c3af10867c56e39c1a9db
mandarker <springnguyen98@gmail.com>
2021-02-04 16:16:11 -0800

    Broken commit with a lot of things missing

---
+++ Chromaturgy/Assets/Scripts/Orbs/Spell Prefabs/YellowSpellController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowSpellController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTransform;
+
+    [SerializeField]
+    private AnimationCurve rotationScale;
+    [SerializeField]
+    private AnimationCurve positionScale;
+
+    [SerializeField]
+    private float rotationSpeed;
+    private float startTime;
+
+    private Vector3 fromPlayer;
+
+    [SerializeField]
+    private float spherePaintRadius;
+
+    [SerializeField]
+    private Color paintColor;
+
+    [SerializeField]
+    private float lifetime;
+
+    void OnEnable()
+    {
+        startTime = Time.time;
+
+        rotationScale.postWrapMode = WrapMode.Loop;
+        positionScale.postWrapMode = WrapMode.Loop;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (int i = 0; i < transform.childCount; i++) {
+            // save the new transformation
+            fromPlayer = transform.GetChild(i).position - playerTransform.position;
+
+            // get correct distance and vector from player first
+            transform.GetChild(i).position = playerTransform.position + fromPlayer.normalized * positionScale.Evaluate((Time.time - startTime) / lifetime);
+
+            // then rotate
+            transform.GetChild(i).RotateAround(playerTransform.position, Vector3.up, rotationScale.Evaluate((Time.time - startTime) / lifetime) * rotationSpeed / fromPlayer.magnitude);
+
+            // where to store globals? eg layermask
+            PaintingManager.PaintSphere(paintColor, transform.GetChild(i).position, spherePaintRadius);
+        }
+    }
+}
---
+++ Chromaturgy/Assets/Scripts/PaintingManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingManager : MonoBehaviour
+{
+    void Start()
+    {
+        // find all paintable gameobjects and set their initial colors
+        GameObject [] objects = FindObjectsOfType<GameObject>();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            // paintable layer
+            if (objects[i].layer == 8)
+            {
+                Debug.Log(objects[i].name);
+                Mesh mesh = objects[i].GetComponent<MeshFilter>().mesh;
+
+                // cloned to prevent multiple referencing by creating a shallow reference
+                List<Vector3> vertices = new List<Vector3>();
+                mesh.GetVertices(vertices);
+
+                List<Color> colors = new List<Color>();
+
+                if (mesh.colors.Length == 0)
+                {
+                    for (int k = 0; k < vertices.Count; k++)
+                        colors.Add(new Color(1, 1, 1, 0));
+                }
+
+                mesh.SetColors(colors);
+            }
+        }
+    }
+
+    public static void PaintSphere(Color paintColor, Vector3 origin, float radius, float threshold = 0.5f)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(origin, radius, 1 << 8);
+
+        foreach (Collider c in hitColliders)
+        {
+            Mesh mesh = c.gameObject.GetComponent<MeshFilter>().mesh;
+            Matrix4x4 localToWorld = c.transform.localToWorldMatrix;
+
+            List<Vector3> vertices = new List<Vector3>();
+            List<Color> colors = new List<Color>();
+            mesh.GetVertices(vertices);
+            mesh.GetColors(colors);
+
+            for (int k = 0; k < colors.Count; k++)
+            {
+                // create vectors for calculations
+                Vector3 worldVertex = localToWorld.MultiplyPoint3x4(vertices[k]);
+
+                if (Vector3.Distance(worldVertex, origin) < radius)
+                {
+                    float l = Vector3.Distance(worldVertex, origin) / radius;
+                    if (l < radius * threshold)
+                        l = 0;
+                    // Perlin smoothstep 
+                    l = l * l * l * (l * (l * 6 - 15) + 10);
+
+                    // jank smoothing calculation to create solid center
+                    // use bitmask for lerp
+                    Color vertexColor = colors[k];
+
+                    vertexColor.r = Mathf.Lerp(paintColor.r, colors[k].r, Mathf.Clamp(1 - (1 - l) * 2, 0, 1));
+                    vertexColor.g = Mathf.Lerp(paintColor.g, colors[k].g, Mathf.Clamp(1 - (1 - l) * 2, 0, 1));
+                    vertexColor.b = Mathf.Lerp(paintColor.b, colors[k].b, Mathf.Clamp(1 - (1 - l) * 2, 0, 1));
+                    vertexColor.a = Mathf.Clamp(1 - l + colors[k].a, 0, 1);
+
+                    colors[k] = vertexColor;
+                }
+            }
+
+            mesh.SetColors(colors);
+        }
+    }
+}
