33acecf366e2de4f2bc54d1b469d2fa3d1dbecde
captnw <williamngoc93@hotmail.com>
2021-03-10 11:22:18 -0800

    Adjusted canvas elements to use anchors, and made the PaintProgressUI slightly more scalable on different aspect ratios

--- Chromaturgy/Assets/Scripts/UI/PaintProgressUI.cs
+++ Chromaturgy/Assets/Scripts/UI/PaintProgressUI.cs
@@ -13,11 +13,13 @@ public class PaintProgressUI : MonoBehaviour
     [SerializeField]
     private RectTransform m_brush;
 
-    [SerializeField]
-    private float m_brushInitialX = 60f;
+    private readonly float m_paintFillGUIPercentWidthScreen = 0.416f;
+    private readonly float m_brushInitialXPercentWidthBar = 0.25f; // percentage of bar width from left to right
+    private readonly float m_brushEndXPercentWidthBar = 0.9583f; // percentage of bar width from left to right
 
-    [SerializeField]
-    private float m_brushTravelLengthX = 146f; // end position is 206? idk UI scales and stuff
+    private float m_brushInitialXPosition;
+    private float m_brushEndXPosition;
+    private float m_brushTravelDistance;
 
     private GameManager m_gmScript;
 
@@ -33,14 +35,19 @@ public class PaintProgressUI : MonoBehaviour
             // destroy this gameObject for nonlevel scenes
             Destroy(gameObject);
         }
-    }
 
+        // paint brush should be placed relative to the position of the PaintBar meter
+        m_brushInitialXPosition = Screen.width * m_paintFillGUIPercentWidthScreen * m_brushInitialXPercentWidthBar;
+        m_brushEndXPosition = Screen.width * m_paintFillGUIPercentWidthScreen * m_brushEndXPercentWidthBar;
+
+        m_brushTravelDistance = m_brushEndXPosition - m_brushInitialXPosition;
+    }
 
     // Update is called once per frame
     private void Update()
     {
         m_paintFillBar.fillAmount = PaintingManager.paintingProgress() / m_gmScript.PaintPercentageNeededToWin;
-        m_brush.position = new Vector2(m_brushInitialX + m_brushTravelLengthX * m_paintFillBar.fillAmount, m_brush.position.y);
+        m_brush.position = new Vector2(m_brushInitialXPosition + m_brushTravelDistance * m_paintFillBar.fillAmount, m_brush.position.y);
     }
 
     #endregion
