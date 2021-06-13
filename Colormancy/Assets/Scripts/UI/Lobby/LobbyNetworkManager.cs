bb01ac54805931c18fa8b7d3778d760a08c7b5e2
William Nguyen <williamngoc93@hotmail.com>
2021-06-13 14:19:08 -0700

    Added support for region switching, and number of players in region are displayed in the lobby. Added game project README

--- Colormancy/Assets/Scripts/UI/Lobby/LobbyNetworkManager.cs
+++ Colormancy/Assets/Scripts/UI/Lobby/LobbyNetworkManager.cs
@@ -6,6 +6,7 @@ using Photon.Realtime;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using MyBox;
+using System;
 
 public class LobbyNetworkManager : MonoBehaviourPunCallbacks
 {
@@ -35,17 +36,28 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
     [SerializeField] private Text m_roomVisibleText;
     [SerializeField] private Text m_roomCapacityText;
 
-    [Separator("Buttons")]
+    [SerializeField] private Text m_regionPlayerCountText; // if connected to lobby, displays the current region you're connected to and the number of players online
+
+    [Separator("Buttons / interactables")]
     [SerializeField] private Button m_leaveRoomButton;
     [SerializeField] private Button m_startGameButton;
     [SerializeField] private Button m_createRoomButton;
     [SerializeField] private Button m_joinRoomButton;
     [SerializeField] private Button m_returnMainMenuButton;
 
+    [SerializeField] private Dropdown m_changeRegionDropdown;
+
+    // these two below are the fake / real labels that display the region name
+    // the fake label is only used when we load into the lobby for the first time
+    // and it correctly displays the region with the best ping for the player
+    // it'll go away if the dropdown is selected
+    private GameObject m_fakeLabel;
+    private GameObject m_realLabel;
+
     [Separator("The main windows/UI (to hide and show if needed)")]
     [SerializeField] private GameObject m_playersMaxPlayersText;
     [SerializeField] private GameObject m_joinButtonGUIText;
-    
+
     [SerializeField] private GameObject m_roomListGUI;
     [SerializeField] private GameObject m_playerListGUI;
 
@@ -53,11 +65,22 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject m_createGameWindow;
     [SerializeField] private GameObject m_joinGameWindow;
 
+    [SerializeField] private GameObject m_changeRegionWindow;
+
     private Coroutine m_createRoomButtonError;
     private Coroutine m_joinRoomButtonError;
 
     private Color m_buttonWhite;
     private Color m_buttonRed = Color.red;
+
+    // the order of regions (in the array bellow) is the same order displayed in the change region dropdown
+    // china is not available (see: https://doc.photonengine.com/en/pun/current/connection-and-authentication/regions#using_the_chinese_mainland_region)
+    private string[] m_photonRegions = new string[] { "asia", "au", "cae", "eu", "in", "jp", "ru", "rue", "za", "sa", "kr", "us", "usw" };
+
+    private bool m_loadingInFromMainMenu = true; // flag for denoting that we loaded from the main menu, and we haven't chosen the region yet (defaults to region with best ping
+
+    [Separator("References to other scripts")]
+    [SerializeField] private RoomListManager m_roomListManager;
 
     #endregion
 
@@ -66,8 +89,52 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
     private void Start()
     {
         m_buttonWhite = m_createRoomButton.GetComponent<Image>().color; // fetch the normal color used by the createRoomButton
+
+        // fetch the labels for the change region dropdown
+        m_fakeLabel = m_changeRegionDropdown.transform.Find("FalseLabel").gameObject;
+        m_realLabel = m_changeRegionDropdown.transform.Find("RealLabel").gameObject;
+
         Initialize();
         Connect();
+    }
+
+    private void Update()
+    {
+        // always update the number of players online and the current region
+        if (PhotonNetwork.InLobby || PhotonNetwork.InRoom)
+        {
+            // Always display this status as long as we're connected to either a lobby or a room
+            string regionName = PhotonNetwork.CloudRegion;
+            int index = regionName.IndexOf("/*");
+
+            //if (index < 0)
+            //{
+            //    print("Region " + regionName + "doesn't have the suffix '/*'");
+            //}
+            //else
+            //{
+            //    print("Region " + regionName + "has the suffix '/*'");
+            //}
+
+            string cleanedRegion = (index < 0) ? regionName : regionName.Remove(index, regionName.Length - index);
+            m_regionPlayerCountText.text = "Region: " + cleanedRegion + " | Players online in this region: " + PhotonNetwork.CountOfPlayers;
+        }
+        else
+        {
+            if (m_regionPlayerCountText.text != "Not connected to either a lobby or a room.")
+            {
+                m_regionPlayerCountText.text = "Not connected to either a lobby or a room.";
+            }
+        }
+
+        if (!PhotonNetwork.InLobby)
+        {
+            // you can only change regions while you're in the lobby
+            if (m_changeRegionDropdown.interactable)
+            {
+                m_changeRegionDropdown.interactable = false;
+            }
+        }
     }
 
     #endregion
@@ -104,6 +171,25 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
     {
         m_statusFieldText.text = "Connected to lobby";
         m_currentLocationText.text = "Rooms";
+
+        // Update the region dropdown to show the fullname of the region
+        if (m_loadingInFromMainMenu)
+        {
+            m_loadingInFromMainMenu = false;
+            m_fakeLabel.GetComponent<Text>().text = m_changeRegionDropdown.options[Array.IndexOf(m_photonRegions, PhotonNetwork.CloudRegion)].text;
+
+            PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = PhotonNetwork.CloudRegion;
+
+            // Initially we had set: PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "" when we loaded into from the menu
+            // this found the best region with the best ping
+            // However we need to disconnect and reconnect again because all of the regions we connect to from the dropdown menu
+            // have a suffix of /*, this makes those regions distinct (i.e: "usw" and "usw/*" are distinct regions)
+            // Having set the FixedRegion variable, reconnecting will place us in the correct region
+            PhotonNetwork.Disconnect();
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        m_changeRegionDropdown.interactable = true;
     }
 
     public override void OnJoinedRoom()
@@ -215,6 +301,40 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
         StartCoroutine(CheckIfYouHaveCreatedRoom());
     }
 
+    /// <summary>
+    /// This callback is for when you click on the dropdown menu to change your region.
+    /// </summary>
+    /// <param name="newRegion">The region index you want to change to.</param>
+    public void OnChangeRegion(int newRegion)
+    {
+        // Ignore the default dropdown (-1) and ignore changing to a region you're already in
+        if (newRegion > -1 && newRegion != Array.IndexOf(m_photonRegions, PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion))
+        {
+            // it's important to disable the button while the player is changing regions, otherwise
+            // complications may occur if a player decides to interact with the dropdown button again
+            m_changeRegionDropdown.gameObject.SetActive(false);
+
+            if (m_fakeLabel.activeSelf)
+            {
+                // Disable the fakeLabel because the value for dropdown won't be -1, the real one can take it from here
+                m_fakeLabel.SetActive(false);
+                m_realLabel.SetActive(true);
+            }
+
+            //print("switching to " + m_photonRegions[newRegion] + " from " + PhotonNetwork.CloudRegion);
+            m_roomListManager.ClearRoomList();
+
+            PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = m_photonRegions[newRegion];
+            PhotonNetwork.Disconnect();
+            PhotonNetwork.ConnectUsingSettings();
+
+            m_changeRegionDropdown.gameObject.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// This callback is for the UI element with the input field (you can specify a room to join), not the join button located on the room UI elements.
+    /// </summary>
     public void OnClickJoinRoom()
     {
         // If not connected, don't create a room
@@ -246,17 +366,27 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
         StartCoroutine(CheckIfYouHaveJoinedTheRoom());
     }
 
+    /// <summary>
+    /// This function returns the player to the main menu after disconnecting from photon.
+    /// </summary>
     public void OnMainMenuButtonPressed()
     {
         PhotonNetwork.Disconnect(); // you need to disconnect when exiting the lobby
         SceneManager.LoadScene("MainMenu");
     }
 
+    /// <summary>
+    /// This function updates the display for the player capacity in your personal room.
+    /// </summary>
+    /// <param name="maxPlayers">The desired player capacity (min is 1, max is 4)</param>
     public void OnSliderUpdatedDisplayMaxPlayerCount(float maxPlayers)
     {
         m_playerCapacityDisplayText.text = "Max players: " + ((int)maxPlayers).ToString();
     }
 
+    /// <summary>
+    /// This function starts the game, can only be invoked if you're the masterclient, and you've created a room.
+    /// </summary>
     public void OnStartGamePressed()
     {
         m_startGameButton.interactable = false; // disable the start and leave room button to prevent race conditions
@@ -374,6 +504,7 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
     {
         if (!PhotonNetwork.IsConnectedAndReady)
         {
+            PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = ""; // coming in from the menu, find the best region with the best ping
             PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.GameVersion = gameVersion;
             PhotonNetwork.AutomaticallySyncScene = true; // important
@@ -415,6 +546,7 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
         m_createGameWindow.SetActive(isRoomList);
         m_changeNameWindow.SetActive(isRoomList);
         m_joinGameWindow.SetActive(isRoomList);
+        m_changeRegionWindow.SetActive(isRoomList);
 
         m_returnMainMenuButton.interactable = isRoomList;
         m_createRoomButton.interactable = isRoomList;
---
+++ README.md
@@ -0,0 +1,127 @@
+# COLORMANCY FINAL BUILD
+#### Last updated 6/13/2021
+---
+### TABLE OF CONTENTS
+
+1) What is Colormancy?
+1) How to play
+2) Known major bugs
+3) Credits
+
+
+### 1) What is Colormancy
+
+Colormancy is a 3D multiplayer action game wherein players play as Chromaturges, powerful magic-wielders who are tasked with bringing the gift of color to worlds which know only gray. These wizards combine colors into an almost limitless variety of powerful spells, which they use to spread color everywhere they go. They are not without opposition, however, as ancient Colorless Ones lurk out in the void, and will not take kindly to the players’ chromatic machinations! 
+
+**_Specification:_**
+
+This is a game project developed for ICS 167 and ICS 168. 
+
+**_Content:_**
++ 9 spells for players to choose (players can hold 2 spells at a time)
++ Working lobby and room system
++ Connect to any region you want
++ 2 Tutorial/dialogue levels and a orb selection level
++ 2 normal levels where you paint the map and fend off against enemies that come from spawners
++ 1 boss cutscene
++ 1 boss level
+
+
+### 2) How to play Colormancy:
+
+##### Utilizing the lobby system
+
+If you want to play a game right away, you can simply create a new room and start the game.
+Here are some features that our lobby system offers:
+
++ changing your username
++ creating rooms
++ displaying rooms with their name and player size and capacity
++ joining rooms (that has room for players)
++ changing your region (play with players in other regions)
+
+
+##### Now that you're in a room...
+
+1) The first level is an intro level, feel free to talk with the NPCs and remember to ready up.
+1) The second level is an orb selection level, everyone must select two orbs and ready up.
+2) The third level is the second intro level, feel free to talk with the NPCs and remember to ready up.
+5) The fourth level is a gameplay level, fend off the enemies while you paint the map. You can see your progress at the top left,
+once that paintbrush reaches the right side of the bar, you will move on to the next level.
+--) Paint the map.
+--) Kill the enemies.
+--) Once you've painted the map, you'll be loaded into the 5th map, where it's a different level, but the goal is the same.
+--) If everyone dies at least once, you're all getting booted back to the lobby
+6) The sixth level is a cutscene level, showcasing how the bosses are going to beat you up.
+7) The seventh level is the boss level, fight off against the bosses. If you win, you win the game!
+
+
+### Actual controls:
+[**_camera_**]
+1. WASD to move around.
+2. Q/E to rotate camera
+3. Scroll forward/ backwards to zoom in /out.
+
+[**_spell/damage_**]
+1. Your normal attack is your left click attack, it's a projectile - try it out. You can also hold down left click to shoot automatically.
+2. Your special attacks would be the spells you've obtain in the lobby (hold right click to aim, then let go to attack).
+3. Spells are assigned to the keys 1/2/3/4/5 ... etc if there is a symbol in the hotbar, there is a spell with the associated number key (starting from 1).
+4. Press the numeric keys to add a "spell" to the queue to the right of your character.
+4b. If the queue is full, adding another spell will dequeue the first spell.
+5. Once you've obtained 3 "spells" in the queue on the right side of your character, right click to see the preview of the effect of the spell, and then let go of the right mouse button to fire the spell.
+6. There are currently no spell cooldown indicators, so you're probably going to have to spam right click.
+7. Spells paint the map and all do damage to enemies (in different ways).
+8. Currently only the first slot of the queue determines the spell that will be cast.
+
+
+### Lobby instructions:
+1) Pick one orb or more orbs by walking up to the orb podiums, clicking on the podium, read the description, and then click next until you see the accept button.
+2) You should see it in your inventory when you picked up an orb.
+3) In this current build, you can pick up an orb multiple times.
+4) Orbs you can pick up: red, orange, yellow, blue, indigo, yellow, violet, green, quicksilver
+
+5) You will also need two players readied up in the lobby to proceed to the first level.
+6) You can spawn as another player by running the unity executable/program again.
+7) You can only ready up if you equip an orb.
+
+
+#### Known major bugs
+- If you leave a game and then rejoin it later, you may spawn outside the map or in a weird area.
+- Enemy desync, sometimes the animations of the enemy doesn't match their behaviour
+- You can one shot the boss with a certain orb
+- This isn't a bug, but there is no in-game volume slider.
+
+### Credits:
++ James David Bates - designer
++ Brigitte Giles - artist
++ Ryan Scott Macwhorter - programmer
++ Spring Nguyen - programmer
++ William Nguyen - programmer
++ Branden Rigo Ulloa - programmer
+
+#### Title screen / UI art assets: 
++ Brigitte Giles
+
+#### Misc. Assets we've used:
++ ADG_Textures
++ Blockout
++ BrokenVector's UltimateDungeonPack
++ Cemetary Kit V1.25
++ Darth_Artisan's Free trees
++ Elf
++ ExplosiveLLC character pack
++ Fantasy Skybox FREE
++ Fantasy Monster (skeleton model and animations)
++ GrassFlowers
++ HalloweenMonstersGhost
++ JKT_ART (bomb model and animations)
++ LowPolyDungeonsLite
++ Photon (networking)
++ Polybrush
++ PolygonDungeon
++ PolygonOffice
++ Starfield
++ TextMesh Pro
++ tuba archmage boss theme
++ tuba mech warrior (and pilot) boss theme
++ Susumu Hirasawa - Berserk OST - 04 Guts
